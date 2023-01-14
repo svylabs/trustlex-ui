aa885ab327a7a412a2b77e14fed6d50cfaa6e33f
Keshav Kishor Ram <ramabishek40@gmail.com>
2023-01-14 15:29:23 +0545

    feat: add navbar in layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Grid } from "@mantine/core";
+import { Grid, Stack } from "@mantine/core";
 import Sidebar from "~/components/Sidebar/Sidebar";
 import styles from "./Layout.module.scss";
-import { Outlet } from "react-router-dom";
-
+import Navbar from "~/components/Navbar/Navbar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Grid className={styles.layout}>
+    <Grid className={styles.layout} gutter={0}>
       <Grid.Col span={"content"}>
         <Sidebar />
       </Grid.Col>
-      <Grid.Col span={11}>{children}</Grid.Col>
+      <Grid.Col span={"auto"}>
+        <Stack>
+          <Navbar />
+          {children}
+        </Stack>
+      </Grid.Col>
     </Grid>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import styles from "./Navbar.module.scss";
+import Button from "~/components/Button/Button";
 type Props = {};
 
 const Navbar = (props: Props) => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.left}>
+        <h2 className={styles.title}>Trustlex</h2>
+        <p className={styles.tagLine}>Non custodial Bitcoin-Crypto exchange</p>
+      </div>
+      <div className={styles.right}>
+        <Button indicator> Bitcoin </Button>
+        <Button indicator> Ethereum </Button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

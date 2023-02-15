72c649bdf39a4f1187b0db6007d4651a3205f9a0
Keshav Kishor Ram <ramabishek40@gmail.com>
2023-02-16 00:31:59 +0545

    generating link modal_hardware

---
+++ src/components/GenerateWalletDrawer/HardwareWallet/HardwareWallet.tsx
@@ -0,0 +1,10 @@
+import styles from "~/components/GenerateWalletDrawer/HardwareWallet/HardwareWallet.module.scss";
+const HardwareWallet = () => {
+  return (
+    <div className={styles.hardwareWalletRoot}>
+      <p>Hardware wallet support coming soon...</p>
+    </div>
+  );
+};
+
+export default HardwareWallet;

ac20d094afe5c40e4b449dae8a97d5e677285929
Keshav Kishor Ram <ramabishek40@gmail.com>
2023-02-17 09:53:46 +0545

    Generate address mobile view

--- src/components/GenerateWalletDrawer/GenerateWalletDrawer.tsx
+++ src/components/GenerateWalletDrawer/GenerateWalletDrawer.tsx
@@ -8,75 +8,46 @@ import Tabs from "~/components/Tabs/Tabs";
 import InstantWallet from "~/components/GenerateWalletDrawer/InstantWallet/InstantWallet";
 import HardwareWallet from "~/components/GenerateWalletDrawer/HardwareWallet/HardwareWallet";
 import useWindowDimensions from "~/hooks/useWindowDimesnsion";
+import useAutoHideScrollbar from "~/hooks/useAutoHideScrollBar";
+import { useRef } from "react";
+import useDetectOutsideClick from "~/hooks/useDetectOutsideClick";
 interface IProps {
   open: boolean;
   onClose: () => void;
 }
 
 const GenerateWalletDrawer = ({ open, onClose }: IProps) => {
-  const { mobileView } = useWindowDimensions();
+  const { mobileView, tabletView } = useWindowDimensions();
+
+  const mobileContentRef = useRef(null);
+  useDetectOutsideClick({
+    ref: mobileContentRef,
+    callback: onClose,
+  });
 
   return mobileView ? (
-    <Dialog
-      opened={open}
-      onClose={onClose}
-      withCloseButton={false}
-      radius="lg"
-      p={0}
-      transition="slide-up"
-      transitionDuration={300}
-      transitionTimingFunction="ease"
-      className={styles.mobileDialog}
-      m={"auto"}
-      style={{ width: "90vw", height: "90vh" }}
-      position={{ top: 70, right: 10, left: 10, bottom: 10 }}
-    >
-      <GradientBackgroundContainer
-        radius={10}
-        colorLeft={"#FEBD3893"}
-        bgImage="/images/Rectangle.svg"
-      >
-        <div className={styles.generateWalletRoot}>
-          <Grid className={styles.heading}>
-            <Grid.Col className={styles.crossIconBox} py={0}>
-              <Button variant={VariantsEnum.default} onClick={onClose} p={0}>
-                <Icon icon="radix-icons:cross-2" className={styles.closeIcon} />
-              </Button>
-            </Grid.Col>
-            <Grid.Col span={12} pt={0}>
-              <Text component="h1" className={styles.headTitle}>
-                Generating Address
-              </Text>
-            </Grid.Col>
-          </Grid>
-          <Tabs
-            tabs={[
-              { label: "Instant wallet", value: "instant-wallet" },
-              { label: "Hardware wallet", value: "hardware-wallet" },
-            ]}
-            panels={[
-              {
-                value: "instant-wallet",
-                children: <InstantWallet />,
-              },
-              {
-                value: "hardware-wallet",
-                children: <HardwareWallet />,
-              },
-            ]}
-          />
+    open ? (
+      <div className={styles.mobileDialogRoot}>
+        <div className={styles.mobileDialogContent} ref={mobileContentRef}>
+          <GradientBackgroundContainer
+            radius={10}
+            colorLeft={"#FEBD3893"}
+            bgImage="/images/Rectangle.svg"
+          >
+            <GenerateWalletContent onClose={onClose} />
+          </GradientBackgroundContainer>
         </div>
-      </GradientBackgroundContainer>
-    </Dialog>
+      </div>
+    ) : null
   ) : (
     <Drawer
       opened={open}
       onClose={onClose}
-      position={"right"}
+      position="right"
       overlayBlur={2.5}
       overlayOpacity={0.5}
       withCloseButton={false}
-      size={600}
+      size={tabletView ? 500 : 600}
       closeOnClickOutside={true}
       closeOnEscape={true}
     >
@@ -85,42 +56,69 @@ const GenerateWalletDrawer = ({ open, onClose }: IProps) => {
         colorLeft={"#FEBD3893"}
         bgImage="/images/Rectangle.svg"
       >
-        <div className={styles.generateWalletRoot}>
-          <Grid className={styles.heading}>
-            <Grid.Col span={11}>
-              <Text component="h1" className={styles.headTitle}>
-                Generating link in browser
-              </Text>
-            </Grid.Col>
-
-            <Grid.Col span={1}>
-              <Button variant={VariantsEnum.default} onClick={onClose} p={0}>
-                <Icon
-                  icon="radix-icons:cross-circled"
-                  className={styles.closeIcon}
-                />
-              </Button>
-            </Grid.Col>
-          </Grid>
-          <Tabs
-            tabs={[
-              { label: "Instant wallet", value: "instant-wallet" },
-              { label: "Hardware wallet", value: "hardware-wallet" },
-            ]}
-            panels={[
-              {
-                value: "instant-wallet",
-                children: <InstantWallet />,
-              },
-              {
-                value: "hardware-wallet",
-                children: <HardwareWallet />,
-              },
-            ]}
-          />
-        </div>
+        <GenerateWalletContent onClose={onClose} />
       </GradientBackgroundContainer>
     </Drawer>
+  );
+};
+
+const GenerateWalletContent = ({ onClose }: { onClose: () => void }) => {
+  const { mobileView } = useWindowDimensions();
+
+  const generateWalletRef = useRef(null);
+  useAutoHideScrollbar(generateWalletRef);
+
+  return (
+    <div className={styles.generateWalletRoot} ref={generateWalletRef}>
+      {mobileView ? (
+        <Grid className={styles.heading}>
+          <Grid.Col className={styles.crossIconBox} py={0}>
+            <Button variant={VariantsEnum.default} onClick={onClose} p={0}>
+              <Icon icon="radix-icons:cross-2" className={styles.closeIcon} />
+            </Button>
+          </Grid.Col>
+          <Grid.Col span={12} pt={0}>
+            <Text component="h1" className={styles.headTitle}>
+              Generating Address
+            </Text>
+          </Grid.Col>
+        </Grid>
+      ) : (
+        <Grid className={styles.heading}>
+          <Grid.Col span={11}>
+            <Text component="h1" className={styles.headTitle}>
+              Generating link in browser
+            </Text>
+          </Grid.Col>
+
+          <Grid.Col span={1}>
+            <Button variant={VariantsEnum.default} onClick={onClose} p={0}>
+              <Icon
+                icon="radix-icons:cross-circled"
+                className={styles.closeIcon}
+              />
+            </Button>
+          </Grid.Col>
+        </Grid>
+      )}
+
+      <Tabs
+        tabs={[
+          { label: "Instant wallet", value: "instant-wallet" },
+          { label: "Hardware wallet", value: "hardware-wallet" },
+        ]}
+        panels={[
+          {
+            value: "instant-wallet",
+            children: <InstantWallet />,
+          },
+          {
+            value: "hardware-wallet",
+            children: <HardwareWallet />,
+          },
+        ]}
+      />
+    </div>
   );
 };
 
